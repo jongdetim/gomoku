24420ffe21621c16c9ef57cf55ef8713730f3bd9
Flint Hignett <fhignett@f0r3s11.codam.nl>
2021-09-20 13:43:01 +0200

    created TT class files

---
+++ includes/TranspositionTable.hpp
@@ -0,0 +1,27 @@
+#ifndef TRANSPOSITION_TABLE_HPP
+# define TRANSPOSITION_TABLE_HPP
+
+# include "Board.hpp"
+# include <unordered_map>
+
+class TableEntry
+{
+    public:
+        int value;
+        int depth;
+        int flag;
+        bool game_finished;
+};
+
+class TranspositionTable
+{
+    private:
+        std::unordered_map<std::bitset<MASKSIZE>, TableEntry> t_table;
+
+    public:
+        bool lookup(Board &node, TableEntry &tt_entry);
+        void insert(Board &node, TableEntry &tt_entry);
+        size_t size();
+};
+
+#endif
---
+++ src/TranspositionTable.cpp
@@ -0,0 +1,26 @@
+#include "TranspositionTable.hpp"
+
+bool		TranspositionTable::lookup(Board &node, TableEntry &tt_entry)
+{
+	std::unordered_map<std::bitset<MASKSIZE>, TableEntry>::const_iterator get;
+
+	get = t_table.find(node.get_state());
+	if (get == t_table.end())
+		return false;
+	else
+	{
+		tt_entry = get->second;
+		// std::cout << "i've seen this state before :)))" << std::endl;
+		return true;
+	}
+}
+
+void		TranspositionTable::insert(Board &node, TableEntry &tt_entry)
+{
+	t_table.insert({node.get_state(), tt_entry});
+}
+
+size_t		TranspositionTable::size()
+{
+	return t_table.size();
+}
