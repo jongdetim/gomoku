fbd2d1a4b1e06acaea6e03bda77485b627edf85e
Flint Louis <flintlouis@Flints-MacBook-Pro.local>
2021-11-05 13:28:05 +0100

    added SDL2 lib

--- src/main.cpp
+++ src/main.cpp
@@ -4,54 +4,63 @@
 
 #define PRINT(x) std::cout << x << std::endl
 
-void create_star(Board &board, int index, int size, int player)
-{
-    for (auto n : NEIGHBOURS)
-    {
-        place_pieces(board, player, index, size, n);
-    }
-}
+#include <iostream>
+#include <SDL2/SDL.h>
 
-int main()
-{
-    Board board;
-    BoardHeuristic heuristic;
-
-    int index = calc_index(3, 16);
-    create_star(board, index, 3, PLAYER1);
-    create_star(board, index, 4, PLAYER2);
-    board.remove(index);
-
-
-    PRINT("Stones played");
-    PRINT(board.get_stones_played());
-    board.print();
-    PRINT("--------------");
-
-    board.place(index, PLAYER2);
-    PRINT("Stones played");
-    PRINT(board.get_stones_played());
-    board.show_last_move();
-    
-    PRINT("--------------");
-
-    index = calc_index(8, 6);
-    create_star(board, index, 3, PLAYER2);
-    create_star(board, index, 4, PLAYER1);
-    board.remove(index);
-
-
-    PRINT("Stones played");
-    PRINT(board.get_stones_played());
-    board.print();
-    PRINT("--------------");
-
-    board.place(index, PLAYER1);
-    PRINT("Stones played");
-    PRINT(board.get_stones_played());
-    board.show_last_move();
-    
-    std::cout << std::endl;
-
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
+
+// void create_star(Board &board, int index, int size, int player)
+// {
+//     for (auto n : NEIGHBOURS)
+//     {
+//         place_pieces(board, player, index, size, n);
+//     }
+// }
+
+// int main()
+// {
+//     Board board;
+//     BoardHeuristic heuristic;
+
+//     int index = calc_index(3, 16);
+//     create_star(board, index, 3, PLAYER1);
+//     create_star(board, index, 4, PLAYER2);
+//     board.remove(index);
+
+
+//     PRINT("Stones played");
+//     PRINT(board.get_stones_played());
+//     board.print();
+//     PRINT("--------------");
+
+//     board.place(index, PLAYER2);
+//     PRINT("Stones played");
+//     PRINT(board.get_stones_played());
+//     board.show_last_move();
+    
+//     PRINT("--------------");
+
+//     index = calc_index(8, 6);
+//     create_star(board, index, 3, PLAYER2);
+//     create_star(board, index, 4, PLAYER1);
+//     board.remove(index);
+
+
+//     PRINT("Stones played");
+//     PRINT(board.get_stones_played());
+//     board.print();
+//     PRINT("--------------");
+
+//     board.place(index, PLAYER1);
+//     PRINT("Stones played");
+//     PRINT(board.get_stones_played());
+//     board.show_last_move();
+    
+//     std::cout << std::endl;
+
+//     return 0;
+// }
