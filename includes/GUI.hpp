dca28b3829dc277694522d3a3711f2f8550e778e
Flint Louis <flintlouis@Flints-MacBook-Pro.local>
2021-11-10 22:59:59 +0100

    added stuff

--- includes/GUI.hpp
+++ includes/GUI.hpp
@@ -10,8 +10,8 @@
 
 enum game_loop
 {
-	quit = -1,
-	loop = 1
+	quit,
+	loop
 };
 
 class GUI
@@ -19,17 +19,19 @@ class GUI
 public:
 	GUI(void);
 	~GUI();
-	bool		initiate_GUI(std::string title, int width, int height);
-	bool		initiate_GUI(std::string title);
-	void		game(const Board &board);
+	bool			initiate_GUI(std::string title, int width, int height);
+	bool			initiate_GUI(std::string title);
+	void			game(const Board &board);
 
 private:
-	SDL_Window	*window;
-	SDL_Surface	*surface;
+	SDL_Window		*window;
+	SDL_Renderer	*renderer;
 
-	void		draw(BITBOARD state);
-	void		update_window(void);
-	game_loop	handle_events(SDL_Event &e);
+	void			draw_state(BITBOARD state);
+	void			update_window(void);
+	game_loop		handle_events(SDL_Event &e);
+
+
 
 };
 
--- src/GUI.cpp
+++ src/GUI.cpp
@@ -12,21 +12,12 @@ bool		GUI::initiate_GUI(std::string title, int width, int height)
         return false;
 	}
 
-	this->window = SDL_CreateWindow(
-        title.c_str(),
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        width,
-        height,
-        SDL_WINDOW_SHOWN
-    );
+	SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_RESIZABLE, &this->window, &this->renderer);
 
 	if (window == NULL) {
         SDL_Log("Unable to create window: %s", SDL_GetError());
         return false;
     }
-
-	this->surface = SDL_GetWindowSurface(this->window);
 
 	return true;
 }
@@ -37,30 +28,80 @@ void		GUI::game(const Board &board)
 
     while (handle_events(e) != quit)
 	{
-		this->draw(board.get_state());
+		this->draw_state(board.get_state());
     }
 }
 
 GUI::~GUI()
 {
-    SDL_DestroyWindow(this->window);
-    SDL_Quit();
+    if (this->renderer)
+		SDL_DestroyRenderer(this->renderer);
+	if (this->window)
+    	SDL_DestroyWindow(this->window);
+	SDL_Quit();
 }
 
 /* Private Methods */
 
-void		GUI::draw(BITBOARD state)
+void		GUI::draw_state(BITBOARD state)
 {
 }
 
-game_loop	GUI::handle_events(SDL_Event &e)
+game_loop	GUI::handle_events(SDL_Event &event)
 {
-	while (SDL_PollEvent(&e) )
+	while (SDL_PollEvent(&event) )
 	{                         
-		if (e.type == SDL_QUIT)
+		if (event.type == SDL_QUIT)
 			return quit;
+		if(event.type == SDL_MOUSEBUTTONUP)
+    	{
+			//Get mouse position
+			int x, y;
+			SDL_GetMouseState( &x, &y );
+			printf("Mouse clicked: X:%d, Y:%d\n", x, y);
+		}
 	}
 	return loop;
 }
 
 void		GUI::update_window(void) { SDL_UpdateWindowSurface(this->window); }
+
+
+
+void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius)
+{
+   const int32_t diameter = (radius * 2);
+
+   int32_t x = (radius - 1);
+   int32_t y = 0;
+   int32_t tx = 1;
+   int32_t ty = 1;
+   int32_t error = (tx - diameter);
+
+   while (x >= y)
+	{
+		//  Each of the following renders an octant of the circle
+		SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+		SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+		SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+		SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+		SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+		SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+		SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+		SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+		if (error <= 0)
+		{
+		++y;
+		error += ty;
+		ty += 2;
+		}
+
+		if (error > 0)
+		{
+		--x;
+		tx += 2;
+		error += (tx - diameter);
+		}
+   }
+}
